d8da93a4eb8235bdf0f6863e2d7733847b6b79d8
BasLee <basleenknegt@gmail.com>
2019-03-21 12:12:25 +0100

    Add font awesome

--- ui/src/app/QueryDocument.tsx
+++ ui/src/app/QueryDocument.tsx
@@ -1,27 +1,33 @@
 import * as React from "react";
-import {Collapse, Button, Card, CardBody, CardHeader} from "reactstrap";
+import {Collapse, Card, CardBody, CardHeader} from "reactstrap";
+import FontAwesome from 'react-fontawesome';
 
 interface QueryDocumentProps {
     id: string;
 }
+
 class QueryDocument extends React.Component<QueryDocumentProps, any> {
     constructor(props: any, context: any) {
         super(props, context);
         this.toggle = this.toggle.bind(this);
-        this.state = { collapse: false };
+        this.state = {collapse: false};
 
     }
 
     toggle() {
-        this.setState({ collapse: !this.state.collapse });
+        this.setState({collapse: !this.state.collapse});
     }
 
     render() {
         return (
             <div>
                 <Card>
-                    <CardHeader className="" onClick={this.toggle} >
-                        <button className="btn btn-primary btn-sm float-right stretched-link">Snippets>></button>
+                    <CardHeader className="" onClick={this.toggle}>
+                        <button className="btn btn-primary btn-sm float-right stretched-link">
+                            <FontAwesome name='plus-square-o' />
+                            &nbsp;
+                            <span>Snippets</span>
+                        </button>
                         {this.props.id}
                     </CardHeader>
                     <Collapse isOpen={this.state.collapse}>
