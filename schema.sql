250572d8740e0e01bc72bc5e7f09d9199e1b81fc
LB <lhuygi@users.noreply.github.com>
2019-04-25 11:58:58 +0200

    Create indexes in SQL schema

--- schema.sql
+++ schema.sql
@@ -3,12 +3,16 @@ PRAGMA foreign_keys = ON;
 CREATE TABLE assessments (
   id TEXT NOT NULL, -- id of assessed snippet
   relevant INTEGER, -- boolean; if NULL, seen but not assessed
-  userid INTEGER,
+  userid INTEGER NOT NULL,
   timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY(userid) REFERENCES users(userid)
 );
+
+CREATE INDEX assessments_by_id ON assessments (id);
 
 CREATE TABLE users (
   userid INTEGER PRIMARY KEY,
   username TEXT UNIQUE NOT NULL
 );
+
+CREATE INDEX users_by_username ON users (username);
