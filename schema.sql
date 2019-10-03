9e31dbe2c95154bf3d314957f0d3cc92f38f5c16
LB <lhuygi@users.noreply.github.com>
2019-10-03 15:30:04 +0200

    Fix SQL schema to allow more than one user to assess a snippet

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE assessments (
-  id TEXT NOT NULL PRIMARY KEY, -- id of assessed snippet
-  relevant INTEGER,             -- boolean; if NULL, seen but not assessed
+  id TEXT NOT NULL, -- id of assessed snippet
+  relevant INTEGER, -- boolean; if NULL, seen but not assessed
   userid INTEGER NOT NULL,
   timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY(id, userid),
   FOREIGN KEY(userid) REFERENCES users(userid)
 ) WITHOUT ROWID;
 
