d4f19a276634fa19a6f24d5881c28daaa6d3af8c
BasLee <basleenknegt@gmail.com>
2019-10-09 14:51:32 +0200

    Replace fa static files with font-awesome5 package

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './fonts/fontawesome-webfont.ttf';
-import './fonts/fontawesome-webfont.woff2';
-import './css/font-awesome.min.css';
+import 'font-awesome5/css/fontawesome-all.css';
 import './css/index.css';
 import App from './app/App';
 import * as serviceWorker from './serviceWorker';
