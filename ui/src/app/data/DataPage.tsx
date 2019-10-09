f7dc81e4049d8b21dff4a158346046cd758a459c
BasLee <basleenknegt@gmail.com>
2019-10-09 14:07:39 +0200

    Add checkbox to export data AND purge database

--- ui/src/app/data/DataPage.tsx
+++ ui/src/app/data/DataPage.tsx
@@ -11,13 +11,15 @@ export default class DataPage extends React.Component<any, any> {
     constructor(props: any, context: any) {
         super(props, context);
         this.state = {
-            purging: false
+            purging: false,
+            savingWithPurge: false,
+            purgeOnSaveFile: true
         };
     }
 
     private saveFile = () => {
         const exportFilename = 'export.csv';
-        Resources.getExport(this.context.user).then((data) => {
+        return Resources.getExport(this.context.user).then((data) => {
             const csv = new Blob([data], {type: "text/csv;charset=utf-8"});
             FileSaver.saveAs(csv, exportFilename);
         }).catch(() => {
@@ -37,23 +39,51 @@ export default class DataPage extends React.Component<any, any> {
         this.setState({purging: true});
     };
 
+    private conformSaveWhenWithPurge = () => {
+        if(this.state.purgeOnSaveFile) {
+            this.setState({savingWithPurge: true});
+        } else {
+            this.saveFile();   
+        }
+    };
+
+    private handleSetPurgeOnSaveFile = () => {
+        this.setState({purgeOnSaveFile: !this.state.purgeOnSaveFile});
+    };
+    private saveFileAndPurge = () => {
+        this.saveFile().then(() => {
+            this.purgeDatabase();
+            this.setState({savingWithPurge: false});
+        });
+    };
+    
     render() {
         return (
             <Page>
                 <h2>Data</h2>
-                <InfoBox msg={this.state.error} type="warning" onClose={() => this.setState({error: null})} />
-                <InfoBox msg={this.state.info} type="info" onClose={() => this.setState({info: null})} />
+                <InfoBox msg={this.state.error} type="warning" onClose={() => this.setState({error: null})}/>
+                <InfoBox msg={this.state.info} type="info" onClose={() => this.setState({info: null})}/>
                 <ul className="list-group d-flex" role="group">
                     <li className="list-group-item list-group-item-action">
                         Exporteer als csv
-                        <button
-                            onClick={this.saveFile}
-                            className="float-right btn btn-info btn-sm"
-                        >
-                            <i className='fa fa-download'/>
-                            &nbsp;
-                            Download
-                        </button>
+                        <div className="btn-group float-right">
+                            <button
+                                className="btn btn-outline-secondary btn-sm"
+                                onClick={this.handleSetPurgeOnSaveFile}
+                            >
+                                Leeg database?
+                                &nbsp;
+                                <i className={`fa ${this.state.purgeOnSaveFile ? 'fa-check-square' :'fa-square-o'}`}/>
+                            </button>
+                            <button
+                                onClick={this.conformSaveWhenWithPurge}
+                                className="btn btn-info btn-sm"
+                            >
+                                <i className='fa fa-download'/>
+                                &nbsp;
+                                Download
+                            </button>
+                        </div>
                     </li>
                     <li className="list-group-item list-group-item-action">
                         Leeg database
@@ -71,6 +101,11 @@ export default class DataPage extends React.Component<any, any> {
                     onCancel={() => this.setState({purging: false})}
                     onContinue={this.purgeDatabase}
                     isOpen={this.state.purging}
+                />
+                <ConfirmModal
+                    onCancel={() => this.setState({savingWithPurge: false})}
+                    onContinue={this.saveFileAndPurge}
+                    isOpen={this.state.savingWithPurge}
                 />
             </Page>
         );
