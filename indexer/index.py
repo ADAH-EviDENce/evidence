4aab2ac0dc7ea0cbf85b874f5ab69d3e3490a6ef
LB <lhuygi@users.noreply.github.com>
2019-10-14 17:12:51 +0200

    Reinstate bulk indexing
    Indexing time is down from minutes to <1s.

--- indexer/index.py
+++ indexer/index.py
@@ -3,10 +3,8 @@
 from collections import defaultdict
 import json
 from io import StringIO
-import os
 import re
 import sys
-import time
 
 import elasticsearch
 
@@ -16,7 +14,8 @@ NUM_PARTS = re.compile(r'(\d+)')
 
 def natural_key(s):
     '''Key function for natural sort.'''
-    return [int(part) if part.isdigit() else part for part in NUM_PARTS.split(s)]
+    return [int(part) if part.isdigit() else part
+            for part in NUM_PARTS.split(s)]
 
 
 es = elasticsearch.Elasticsearch(*sys.argv[3:])
@@ -61,23 +60,31 @@ es.indices.create('snippets', body={
 
 snippets = defaultdict(set)
 
-print('Indexing...')
+print('Indexing snippets...')
 with open(sys.argv[1]) as idfile, open(sys.argv[2]) as textfile:
+    data = StringIO()
+
     for ident, text in zip(idfile, textfile):
         ident = ident.strip()
+        json.dump({'index': {'_id': ident}}, data)
+        data.write('\n')
 
         doc, _ = ident.split('_paragraph', 1)
         snippets[doc].add(ident)
 
-        es.index(index='snippets', doc_type='snippet', id=ident, body={
-            'text': text, 'document': doc,
-        })
-        print(ident)
+        json.dump({'text': text, 'document': doc}, data)
+        data.write('\n')
 
+    es.bulk(index='snippets', doc_type='snippet', body=data.getvalue())
+
+print('Indexing documents...')
+data = StringIO()
 for doc, snippetset in snippets.items():
-    es.index(index='documents', doc_type='document', body={
-        'sub': sorted(snippetset),
-    })
-    print(doc)
+    json.dump({'index': {'_id': doc}}, data)
+    data.write('\n')
+    json.dump({'sub': sorted(snippetset)}, data)
+    data.write('\n')
 
-print('... done.')
+es.bulk(index='documents', doc_type='document', body=data.getvalue())
+
+print('Indexing done.')
