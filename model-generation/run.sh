872fdcab457125a212ae126fe9a39430710ff91a
Faruk D <f.diblen@esciencecenter.nl>
2020-07-16 16:28:55 +0200

    progress bar work on the terminal

--- model-generation/run.sh
+++ model-generation/run.sh
@@ -1,11 +1,11 @@
-#! /bin/bash
+#!/bin/bash
+
+set -e
 
 echo "starting corpus preprocessing"
-
-jupyter nbconvert --to notebook --execute preprocess_corpus.ipynb && rm preprocess_corpus.nbconvert.ipynb || exit 1
+jupyter nbconvert --to python preprocess_corpus.ipynb
+python preprocess_corpus.py && rm -f preprocess_corpus.py
 
 echo "starting doc2vec model generation - this takes a few minutes"
-
-jupyter nbconvert --to notebook --execute generate_doc2vec_model.ipynb --ExecutePreprocessor.timeout=3600 && rm generate_doc2vec_model.nbconvert.ipynb || exit 1
-
-echo "done"
+jupyter nbconvert --to python generate_doc2vec_model.ipynb
+python generate_doc2vec_model.py && rm -f generate_doc2vec_model.py
