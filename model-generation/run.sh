ce835ed63283b3340a8e996b35fc123ac37aedc7
Faruk D <f.diblen@esciencecenter.nl>
2020-07-16 17:12:09 +0200

    activate environment in entrypoint script and fix Python version

--- model-generation/run.sh
+++ model-generation/run.sh
@@ -2,10 +2,13 @@
 
 set -e
 
+source /opt/conda/bin/activate evidence
+echo "Using Python: "$(which python)
+
 echo "starting corpus preprocessing"
 jupyter nbconvert --to python preprocess_corpus.ipynb
-python preprocess_corpus.py && rm -f preprocess_corpus.py
+python3 preprocess_corpus.py && rm -f preprocess_corpus.py
 
 echo "starting doc2vec model generation - this takes a few minutes"
 jupyter nbconvert --to python generate_doc2vec_model.ipynb
-python generate_doc2vec_model.py && rm -f generate_doc2vec_model.py
+python3 generate_doc2vec_model.py && rm -f generate_doc2vec_model.py
